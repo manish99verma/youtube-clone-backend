6613ceba2100fa059d0a15ec62e6a15d0e1057c5
manish99verma <manish6203993@gmail.com>
2024-08-29 12:45:21 +0530

    connect mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+const app = express();
+
 console.log("Starting server...");
+
+dotenv.config({ path: "./env" });
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on  port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR", error);
+    throw error;
+  }
+})();
+*/
+
+connectDB();
